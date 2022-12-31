e0ef9e9a84d99621bef1c13d81d542024696fc24
Eren A. Akyol <ahmederen123@gmail.com>
2022-12-31 13:53:40 +0300

    remake

---
+++ app/AGGridDataBuilder.php
@@ -0,0 +1,235 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use function array_key_exists;
+use function array_push;
+use function count;
+use function is_null;
+use function join;
+use function sizeof;
+
+/**
+ * AgGridDataBuilder for Laravel Eloquent. Made by @xerenahmed
+ */
+class AGGridDataBuilder {
+	private int $rowCount;
+	private Collection $resultsForPage;
+	private Collection $results;
+
+	private function __construct(private Builder $sqlBuilder) {
+	}
+
+	public static function create(string $modelClass) : self {
+		return new self($modelClass::query());
+	}
+
+	public function build(Request $request) : self {
+		$this->applySelect($request);
+		$this->applyFilters($request);
+		$this->applyGrouping($request);
+		$this->applySorting($request);
+		$this->applyLimit($request);
+
+		\Log::debug($this->sqlBuilder->toSql());
+		$this->fetch();
+		$this->rowCount = $this->initRowCount($request);
+		$this->resultsForPage = $this->cutResultsToPageSize($request);
+
+		return $this;
+	}
+
+	protected function fetch() : self {
+		$this->results = $this->sqlBuilder->get();
+		return $this;
+	}
+
+	private function applySelect(Request $request) : self {
+		$rowGroupCols = $request->input('rowGroupCols');
+		$valueCols = $request->input('valueCols');
+		$groupKeys = $request->input('groupKeys');
+
+		if (!(count($rowGroupCols) > count($groupKeys))) {
+			return $this;
+		}
+
+		$colsToSelect = [];
+
+		$rowGroupCol = $rowGroupCols[sizeof($groupKeys)];
+		array_push($colsToSelect, $rowGroupCol['field']);
+
+		foreach ($valueCols as $_ => $value) {
+			array_push($colsToSelect, $value['aggFunc'] . '(' . $value['field'] . ') as ' . $value['field']);
+		}
+
+		$this->sqlBuilder->select(new Expression(join(',', $colsToSelect)));
+		return $this;
+	}
+
+	private function applyGrouping(Request $request) : self {
+		$rowGroupCols = $request->input('rowGroupCols');
+		$groupKeys = $request->input('groupKeys');
+
+		if (empty($groupKeys) || empty($rowGroupCols) || count($groupKeys) >= count($rowGroupCols)) {
+			if (count($rowGroupCols) > count($groupKeys)) {
+				$this->sqlBuilder->groupBy($rowGroupCols[sizeof($groupKeys)]['field']);
+			}
+			return $this;
+		}
+
+		$colsToGroupBy = [];
+		$rowGroupCol = $rowGroupCols[sizeof($groupKeys) - 1];
+		array_push($colsToGroupBy, $rowGroupCol['field']);
+
+		$this->sqlBuilder->groupBy($colsToGroupBy);
+		return $this;
+	}
+
+	private function applySorting(Request $request) : self {
+		$sortModel = $request->input('sortModel');
+		if (empty($sortModel)) {
+			return $this;
+		}
+
+		foreach ($sortModel as $sort) {
+			$this->sqlBuilder->orderBy($sort['colId'], $sort['sort']);
+		}
+
+		return $this;
+	}
+
+	private function applyLimit(Request $request) : self {
+		$startRow = $request->input('startRow');
+		$endRow = $request->input('endRow');
+		$pageSize = ($endRow - $startRow) + 1;
+
+		$this->sqlBuilder->limit($pageSize)->offset($startRow);
+		return $this;
+	}
+
+	private function applyFilters(Request $request) : self {
+		$filters = $request->get('filterModel');
+		$rowGroupCols = $request->input('rowGroupCols');
+		$groupKeys = $request->input('groupKeys');
+
+		if (sizeof($groupKeys) > 0) {
+			foreach ($groupKeys as $key => $value) {
+				$colName = $rowGroupCols[$key]['field'];
+				$this->sqlBuilder->where($colName, $value);
+			}
+		}
+
+		if (empty($filters)) {
+			return $this;
+		}
+
+		foreach ($filters as $field => $data) {
+			if (array_key_exists('operator', $data)) {
+				$operator = $data['operator'];
+				$conditionData1 = $data['condition1'];
+				$conditionData2 = $data['condition2'];
+
+				$this->sqlBuilder->where(function (Builder $query) use ($field, $conditionData1, $conditionData2, $operator) {
+					$this->handleWhereFor($query, $field, $conditionData1);
+					$this->handleWhereFor($query, $field, $conditionData2, $operator === 'OR');
+				});
+			} else {
+				$this->handleWhereFor($this->sqlBuilder, $field, $data);
+			}
+		}
+
+		return $this;
+	}
+
+	private function handleWhereFor(QueryBuilder|Builder $builder, string $field, array $data, bool $isOr = false) : void {
+		$type = $data['type'];
+		if ($type === 'inRange') {
+			$builder->{$isOr ? 'orWhereBetween' : 'whereBetween'}($field, [$data['filter'], $data['filterTo']]);
+			return;
+		}
+
+		if ($type === 'blank') {
+			$builder->{$isOr ? 'orWhereNull' : 'whereNull'}($field);
+			return;
+		}
+
+		if ($type === 'notBlank') {
+			$builder->{$isOr ? 'orWhereNotNull' : 'whereNotNull'}($field);
+			return;
+		}
+
+		$filter = $data['filter'];
+		$value = match ($type) {
+			'contains', 'notContains' => '%' . $filter . '%',
+			'startsWith' => $filter . '%',
+			'endsWith' => '%' . $filter,
+			default => $filter,
+		};
+
+		$operator = match ($type) {
+			'equals' => '=',
+			'notEqual' => '!=',
+			'lessThan' => '<',
+			'lessThanOrEqual' => '<=',
+			'greaterThan' => '>',
+			'greaterThanOrEqual' => '>=',
+			'contains', 'startsWith', 'endsWith' => 'LIKE',
+			'notContains' => 'NOT LIKE',
+			default => '=',
+		};
+
+		$builder->{$isOr ? 'orWhere' : 'where'}($field, $operator, $value);
+	}
+
+	private function initRowCount($request) {
+		$results = $this->results;
+		if (is_null($results) || !isset($results) || sizeof($results) == 0) {
+			// or return null
+			return 0;
+		}
+
+		$currentLastRow = $request['startRow'] + sizeof($results);
+
+		if ($currentLastRow <= $request['endRow']) {
+			return $currentLastRow;
+		} else {
+			return -1;
+		}
+	}
+
+	private function cutResultsToPageSize($request) : Collection {
+		$results = $this->results;
+		$pageSize = $request['endRow'] - $request['startRow'];
+		if ($results && (sizeof($results) > $pageSize)) {
+			return $results->slice($request['startRow'], $pageSize);
+		} else {
+			return $results;
+		}
+	}
+
+	public function getResultsForPage() : Collection {
+		return $this->resultsForPage;
+	}
+
+	public function getRowCount() : int {
+		return $this->rowCount;
+	}
+
+	public function asResponse() : array {
+		return [
+			'lastRow' => $this->rowCount,
+			'rows' => $this->resultsForPage,
+		];
+	}
+
+	public function getResults() : Collection {
+		return $this->results;
+	}
+}
--- app/Http/Controllers/AthleteController.php
+++ app/Http/Controllers/AthleteController.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\AGGridDataBuilder;
+use App\Models\Athlete;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use function array_push;
-use function array_splice;
-use function is_null;
-use function join;
-use function sizeof;
 
 class AthleteController extends Controller {
 	public function getSetFilterValues(Request $request, $field) {
@@ -19,148 +16,8 @@ class AthleteController extends Controller {
 	}
 
 	public function getData(Request $request) {
-		$SQL = $this->buildSql($request);
-		// for debugging purposes - logs are saved to storage/logs/laravel.log
-		$results = DB::select($SQL);
-		$rowCount = $this->getRowCount($request, $results);
-		$resultsForPage = $this->cutResultsToPageSize($request, $results);
-		return ['rows' => $resultsForPage, 'lastRow' => $rowCount];
-	}
-
-	public function buildSql(Request $request) {
-		$selectSql = $this->createSelectSql($request);
-		$fromSql = " FROM athletes ";
-		$whereSql = $this->whereSql($request);
-		$groupBySql = $this->groupBySql($request);
-		$orderBySql = $this->orderBySql($request);
-		$limitSql = $this->createLimitSql($request);
-
-		$SQL = $selectSql . $fromSql . $whereSql . $groupBySql . $orderBySql . $limitSql;
-		return $SQL;
-	}
-
-	public function createSelectSql(Request $request) {
-		$rowGroupCols = $request->input('rowGroupCols');
-		$valueCols = $request->input('valueCols');
-		$groupKeys = $request->input('groupKeys');
-
-		if ($this->isDoingGrouping($rowGroupCols, $groupKeys)) {
-			$colsToSelect = [];
-
-			$rowGroupCol = $rowGroupCols[sizeof($groupKeys)];
-			array_push($colsToSelect, $rowGroupCol['field']);
-
-			foreach ($valueCols as $key => $value) {
-				array_push($colsToSelect, $value['aggFunc'] . '(' . $value['field'] . ') as ' . $value['field']);
-			}
-
-			return "SELECT " . join(", ", $colsToSelect);
-		}
-
-		return "SELECT * ";
-	}
-
-	public function whereSql(Request $request) {
-		$rowGroupCols = $request->input('rowGroupCols');
-		$groupKeys = $request->input('groupKeys');
-		$filterModel = $request->input('filterModel');
-
-		$whereParts = [];
-
-		if (sizeof($groupKeys) > 0) {
-			foreach ($groupKeys as $key => $value) {
-				$colName = $rowGroupCols[$key]['field'];
-				array_push($whereParts, "{$colName} = '{$value}'");
-			}
-		}
-
-		if ($filterModel) {
-			foreach ($filterModel as $key => $value) {
-				if ($value['filterType'] == 'set') {
-					array_push($whereParts, $key . ' IN ("' . join('", "', $value['values']) . '")');
-				}
-			}
-		}
-
-		if (sizeof($whereParts) > 0) {
-			return " WHERE " . join(' and ', $whereParts);
-		} else {
-			return "";
-		}
-	}
-
-	public function groupBySql(Request $request) {
-		$rowGroupCols = $request->input('rowGroupCols');
-		$groupKeys = $request->input('groupKeys');
-
-		if ($this->isDoingGrouping($rowGroupCols, $groupKeys)) {
-			$colsToGroupBy = [];
-
-			$rowGroupCol = $rowGroupCols[sizeof($groupKeys)];
-			array_push($colsToGroupBy, $rowGroupCol['field']);
-
-			return " GROUP BY " . join(", ", $colsToGroupBy);
-		} else {
-			// select all columns
-			return "";
-		}
-	}
-
-	public function orderBySql(Request $request) {
-		$sortModel = $request->input('sortModel');
-
-		if ($sortModel) {
-			$sortParts = [];
-
-			foreach ($sortModel as $key => $value) {
-				array_push($sortParts, $value['colId'] . " " . $value['sort']);
-			}
-
-			if (sizeof($sortParts) > 0) {
-				return " ORDER BY " . join(", ", $sortParts);
-			} else {
-				return '';
-			}
-		}
-	}
-
-	public function isDoingGrouping($rowGroupCols, $groupKeys) {
-		// we are not doing grouping if at the lowest level. we are at the lowest level
-		// if we are grouping by more columns than we have keys for (that means the user
-		// has not expanded a lowest level group, OR we are not grouping at all).
-		return sizeof($rowGroupCols) > sizeof($groupKeys);
-	}
-
-	public function createLimitSql(Request $request) {
-		$startRow = $request->input('startRow');
-		$endRow = $request->input('endRow');
-		$pageSize = ($endRow - $startRow) + 1;
-
-		return " LIMIT {$pageSize} OFFSET {$startRow};";
-	}
-
-	public function getRowCount($request, $results) {
-		if (is_null($results) || !isset($results) || sizeof($results) == 0) {
-			// or return null
-			return 0;
-		}
-
-		$currentLastRow = $request['startRow'] + sizeof($results);
-
-		if ($currentLastRow <= $request['endRow']) {
-			return $currentLastRow;
-		} else {
-			return -1;
-		}
-	}
-
-	public function cutResultsToPageSize($request, $results) {
-		$pageSize = $request['endRow'] - $request['startRow'];
-
-		if ($results && (sizeof($results) > $pageSize)) {
-			return array_splice($results, 0, $pageSize);
-		} else {
-			return $results;
-		}
+		return AGGridDataBuilder::create(Athlete::class)
+			->build($request)
+			->asResponse();
 	}
 }
